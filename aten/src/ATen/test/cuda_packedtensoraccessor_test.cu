269e194d485396711bef7d0146fd09654e01ffd2
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2019-10-25 08:53:08 -0700

    Checkpoint

--- aten/src/ATen/test/cuda_packedtensoraccessor_test.cu
+++ aten/src/ATen/test/cuda_packedtensoraccessor_test.cu
@@ -9,9 +9,9 @@
 using namespace at;
 
 __global__ void test_tensor_packed_accessor_kernel(
-    NestedTensorAccessor64<float, 1, RestrictPtrTraits> resa,
-    NestedTensorAccessor64<float, 2, RestrictPtrTraits> t1a,
-    NestedTensorAccessor64<float, 1, RestrictPtrTraits> t2a) {
+    PackedTensorAccessor64<float, 1, RestrictPtrTraits> resa,
+    PackedTensorAccessor64<float, 2, RestrictPtrTraits> t1a,
+    PackedTensorAccessor64<float, 1, RestrictPtrTraits> t2a) {
   for (int64_t i = 0; i < resa.size(0); i++) {
     float val = 0.0f;
     for (int64_t j = 0; j < t1a.size(1); j++) {
@@ -21,7 +21,7 @@ __global__ void test_tensor_packed_accessor_kernel(
   }
 }
 
-// test GenericNestedTensorAccessor and Tensor.generic_packed_accessor
+// test GenericPackedTensorAccessor and Tensor.generic_packed_accessor
 TEST(PackedtensoraccessorTest, PackedtensoraccessorTestCUDA) {
   if (!at::cuda::is_available()) return;
   manual_seed(123);
