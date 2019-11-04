8442afcc6ef343bb4148caa18e8773d58f102ea1
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2019-11-04 14:02:14 -0800

    Checkpoint

--- torch/csrc/autograd/python_nested_tensor.h
+++ torch/csrc/autograd/python_nested_tensor.h
@@ -4,35 +4,35 @@
 #include <memory>
 #include <ATen/ATen.h>
 
-#include <torch/csrc/autograd/variable.h>
+#include <torch/csrc/autograd/nested_tensor.h>
 #include <torch/csrc/THP_export.h>
 
-// Python object that backs torch.autograd.Variable
+// Python object that backs torch.autograd.NestedTensor
 // NOLINTNEXTLINE(cppcoreguidelines-pro-type-member-init)
-struct THPVariable {
+struct THPNestedTensor {
     PyObject_HEAD
     // Payload
-    torch::autograd::Variable cdata;
+    torch::autograd::NestedTensor cdata;
     // Hooks to be run on backwards pass (corresponds to Python attr
     // '_backwards_hooks', set by 'register_hook')
     PyObject* backward_hooks = nullptr;
 };
 
-THP_API PyObject *THPVariableClass;
+THP_API PyObject *THPNestedTensorClass;
 
-bool THPVariable_initModule(PyObject *module);
-THP_API PyObject * THPVariable_Wrap(torch::autograd::Variable var);
+bool THPNestedTensor_initModule(PyObject *module);
+THP_API PyObject * THPNestedTensor_Wrap(torch::autograd::NestedTensor var);
 
-static inline bool THPVariable_CheckExact(PyObject *obj) {
-  return Py_TYPE(obj) == (PyTypeObject*)THPVariableClass;
+static inline bool THPNestedTensor_CheckExact(PyObject *obj) {
+  return Py_TYPE(obj) == (PyTypeObject*)THPNestedTensorClass;
 }
 
-inline bool THPVariable_Check(PyObject *obj)
+inline bool THPNestedTensor_Check(PyObject *obj)
 {
-  return THPVariableClass && PyObject_IsInstance(obj, THPVariableClass);
+  return THPNestedTensorClass && PyObject_IsInstance(obj, THPNestedTensorClass);
 }
 
-inline torch::autograd::Variable& THPVariable_Unpack(PyObject* obj) {
-  auto var = (THPVariable*)obj;
+inline torch::autograd::NestedTensor& THPNestedTensor_Unpack(PyObject* obj) {
+  auto var = (THPNestedTensor*)obj;
   return var->cdata;
 }
