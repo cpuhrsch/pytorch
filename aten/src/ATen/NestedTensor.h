43b00fe71394d6374531204f8abaee1b2de44c69
Christian Puhrsch <cpuhrsch@fb.com>
2020-03-27 02:26:02 -0400

    Checkpoint

--- aten/src/ATen/NestedTensor.h
+++ aten/src/ATen/NestedTensor.h
@@ -8,26 +8,18 @@ namespace at {
 constexpr auto NestedTensorKey = DispatchKey::NestedTensorId;
 
 struct NestedTensorImpl : public c10::TensorImpl {
-  explicit NestedTensorImpl(
-      torch::nested_tensor::NestedTensor rep)
-    : TensorImpl(
-        c10::DispatchKeySet(NestedTensorKey),
-        rep.dtype(),
-        rep.device()
-      )
-    , rep_(std::move(rep))
-    {
-  std::cout << "DOING THIS" << std::endl;
-    }
+  explicit NestedTensorImpl(torch::nested_tensor::NestedTensor rep)
+      : TensorImpl(
+            c10::DispatchKeySet(NestedTensorKey),
+            rep.dtype(),
+            rep.device()),
+        rep_(std::move(rep)) {}
 
   int64_t dim() const {
-    std::cout << "GETTING DIM" << std::endl;
     return rep_.dim();
   }
 
-
   IntArrayRef sizes() const {
-  std::cout << "DOING THAT" << std::endl;
     std::vector<c10::optional<int64_t>> size = rep_.size();
     std::vector<int64_t> sizes;
     for (auto opt_int : size) {
